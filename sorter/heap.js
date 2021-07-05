6da27dc03edddb1ab62115582b417153ffb6c101
danghualong <dhl_345@qq.com>
2021-07-05 23:50:37 +0800

    增加堆排序

--- sorter/heap.js
+++ sorter/heap.js
@@ -1,67 +1,67 @@
-var Node=function(key,val){
-    this.key=key;
-    this.val=val;
+// 交换元素
+var swap = (items, i, j) => {
+    let tmp = items[i];
+    items[i] = items[j];
+    items[j] = tmp;
 };
-var swap=(items,i,j)=>{
-    let tmp=items[i];
-    items[i]=items[j];
-    items[j]=tmp;
-};
-
-var sort=(items, k)=>{
-    
-    var heapify=(items)=>{
-        let n=items.length;
-        let curIdx=parseInt(n/2)-1;
-        while(curIdx>=0){
-            sink(items,curIdx,n);
-            curIdx--;
-        }
-    };
-    var sink=(items,curIndex,endIndex)=>{
-        if(curIndex>=endIndex){
-            return;
-        }
-        let i=curIndex;
-        let lidx=curIndex*2+1;
-        let ridx=curIndex*2+2;
-        if(lidx<endIndex && items[i].val<items[lidx].val){
-            i=lidx;
-        }
-        if(ridx<endIndex && items[i].val<items[ridx].val){
-            i=ridx;
-        }
-        if(i!=curIndex){
-            swap(items,i,curIndex);
-            sink(items,i,endIndex);
-        }
-    };
-    
-    heapify(items);
-    let n=items.length;
-    let result=[];
-    let stopIndex=(n>=k)?n-k:0;
-    for(let i=n-1;i>=stopIndex;i--){
-        swap(items,0,i);
-        result.push(items[i].key);
-        sink(items,0,i);
+// 比较元素大小
+function compare(items, first, second, reverse) {
+    if (reverse) {
+        return items[first] > items[second];
+    } else {
+        return items[first] < items[second];
     }
-    return result;
-};
-
-let nums=[1,1,1,2,2,2,3,3,3,3,5,4];
-let map=new Map();
-for(let i=0;i<nums.length;i++){
-    if(!map.has(nums[i])){
-        map.set(nums[i],0);
+}
+// 下沉当前节点
+// i为当前节点索引
+// k为最大的索引位置
+// reverse 是否倒序
+function sink(items, i, k, reverse) {
+    while (i < k) {
+        let target = i;
+        if (i * 2 + 1 < k && compare(items, target, i * 2 + 1, reverse)) {
+            target = i * 2 + 1;
+        }
+        if (i * 2 + 2 < k && compare(items, target, i * 2 + 2, reverse)) {
+            target = i * 2 + 2;
+        }
+        if (target == i) {
+            break;
+        }
+        swap(items, i, target);
+        sink(items, target, k, reverse);
     }
-    map.set(nums[i],map.get(nums[i])+1);
 }
 
-let items=[];
-for(let item of map){
-    items.push(new Node(item[0],item[1]));
+// 构建k个元素的堆
+function heapify(items, k, reverse) {
+    if (k <= 0) {
+        return;
+    }
+    if (k > items.length) {
+        k = items.length;
+    }
+    // 从最后一个拥有孩子的节点开始，直到根节点
+    for (let i = parseInt((k - 2) / 2); i >= 0; i--) {
+        sink(items, i, k, reverse);
+    }
+}
+// 排列元素
+// reverse是否倒序排列
+function sort(items, reverse) {
+    let k = items.length;
+    // 构建堆
+    heapify(items, k, reverse);
+    while (k > 0) {
+        // 交换最大值和第k个值，
+        // 保证最大值在最后面
+        swap(items, 0, k - 1);
+        // 下沉第1个节点
+        sink(items, 0, k - 1, reverse);
+        k--;
+    }
 }
 
-let result=sort(items,2);
-console.log(result);
+const items = [4, 7, 3, 10, 5, 2, 1, 9, 9];
+sort(items, true);
+console.log(items);
